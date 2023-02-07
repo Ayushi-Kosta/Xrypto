8c40280e0a82af1431d643a9d684c0177f1359d9
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2023-02-08 01:42:45 +0530

    api key updated

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const APIKEY = import.meta.env.VITE_GIPHY_API;
+const APIKEY = process.env.VITE_GIPHY_API;
 
 const useFetch = ({ keyword }) => {
   const [gifUrl, setGifUrl] = useState("");
